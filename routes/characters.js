53331b594f946a26b4c18a73fa69c6240ac71cb8
Ben Bryant <brbryant2639@gmail.com>
2021-12-09 17:20:36 -0600

    UPDATE: fixed scraper

--- routes/characters.js
+++ routes/characters.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const { readFile } = require("fs");
 const path = require("path");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
   try {
-    res.sendFile(path.join(__dirname, "../assets/characters.json"));
+    let charactersFile = path.join(__dirname, "../characters.json");
+    let { name } = req.query;
+
+    if (name) {
+      readFile(charactersFile, (err, characters) => {
+        if (err) next(err);
+
+        let char = JSON.parse(characters.toString()).find(
+          (val) => val.name.toLowerCase() == name.toLowerCase()
+        );
+
+        if (char) res.json(char);
+        else res.json({ msg: `No character found for ${name}` });
+      });
+    } else {
+      res.sendFile(charactersFile);
+    }
   } catch (err) {
     next(err);
   }
--- utils/scraper.js
+++ utils/scraper.js
@@ -1,7 +1,10 @@
 const cheerio = require("cheerio");
 const axios = require("axios").default;
+const { writeFile } = require("fs");
 
 let baseURL = "https://kingkiller.fandom.com";
+let characters = [];
+let expectedLength = 0;
 
 async function pullCharacters() {
   try {
@@ -9,20 +12,67 @@ async function pullCharacters() {
       "https://kingkiller.fandom.com/wiki/Category:Characters"
     );
     let $ = cheerio.load(data);
-    for (let ele of $("li.category-page__member > a")) {
-      pullSingleCharacter(baseURL + ele.attribs.href);
+    let charactersHTML = $("li.category-page__member > a");
+    expectedLength = charactersHTML.length;
+    for (let ele of charactersHTML) {
+      if (!$(ele).text().includes("Category")) {
+        pullSingleCharacter(baseURL + ele.attribs.href, $(ele).text());
+      } else {
+        expectedLength--;
+      }
     }
+    // pullSingleCharacter(
+    //   baseURL + $("li.category-page__member > a")[0].attribs.href
+    // );
   } catch (e) {
     console.error(e);
   }
 }
 
-async function pullSingleCharacter(url) {
+async function pullSingleCharacter(url, title) {
   let { data } = await axios.get(url);
+  let char = {};
   let $ = cheerio.load(data);
-  for (let ele of $(".pi-data")) {
-    console.log(ele);
+  for (let ele of $("aside")[0].children) {
+    if ($(ele).attr("data-source") == "name") {
+      char[$(ele).attr("data-source")] = $(ele).text();
+    } else if (ele.name == "section") {
+      let details = {};
+      let extracted = [];
+
+      let divs = ele.children.filter((ele) => ele.name == "div");
+
+      for (let div of divs) {
+        for (let prop of div.children) {
+          if ($(prop).text().replace(/\n|\t/g, "")) {
+            extracted.push($(prop).text());
+          }
+        }
+      }
+
+      for (let i = 0; i < extracted.length; i++) {
+        if (i % 2 == 0) {
+          details[extracted[i]] = extracted[i + 1];
+        }
+      }
+      char[ele.children[1].children[0].data] = details;
+    }
   }
+
+  if (Object.keys(char).length == 0) {
+    char.name = title;
+    char.details = "No details";
+  }
+
+  characters.push(char);
+  console.log(characters.length, expectedLength);
+
+  if (characters.length == expectedLength)
+    writeFile("./characters.json", JSON.stringify(characters), (err) => {
+      if (err) return console.error(err);
+
+      console.log("Overwrote characters.json file ✅");
+    });
 }
 
 pullCharacters();
